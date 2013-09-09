cd5223e26481b0ce95d9de101b6e1684be3c3daf
moonlightly <906268147@qq.com>
2013-09-10 01:50:30 +0800

    initial commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,95 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Skip droiddoc build to save build time
+BOARD_SKIP_ANDROID_DOC_BUILD := true
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := false
+COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
+
+#Camera
+USE_CAMERA_STUB := false
+CAMERA_USES_SURFACEFLINGER_CLIENT_STUB := true
+BOARD_HAVE_HTC_FFC := true
+BOARD_CAMERA_HAVE_ISO := true
+COMMON_GLOBAL_CFLAGS += -DHAVE_ISO
+COMMON_GLOBAL_CFLAGS += -DMR0_CAMERA_BLOB
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
+
+# Board 
+TARGET_BOARD_PLATFORM := tegra
+TARGET_NO_BOOTLOADER := true
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a9
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_HAVE_32_BYTE_CACHE_LINES := true
+ARCH_ARM_USE_NON_NEON_MEMCPY := true
+
+# Optimization build flags
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
+
+# Board nameing
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOOTLOADER_BOARD_NAME := 
+TARGET_BOARD_PLATFORM := tegra
+
+# Enable WEBGL in WebKit
+ENABLE_WEBGL := true
+
+# EGL settings
+USE_OPENGL_RENDERER := true
+BOARD_EGL_NEEDS_LEGACY_FB := true
+BOARD_EGL_CFG := device/htc/tegra3-common/configs/egl.cfg
+
+# BT
+BOARD_HAVE_BLUETOOTH := true
+
+# USB
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 22
+BOARD_HAS_SDCARD_INTERNAL := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun0/file"
+
+# HTC specific
+BOARD_USE_NEW_LIBRIL_HTC := true
+
+# HTCLOG
+COMMON_GLOBAL_CFLAGS += -DHTCLOG
+
+# SE Linux policies
+BOARD_SEPOLICY_DIRS := \
+    device/htc/tegra3-common/selinux
+
+BOARD_SEPOLICY_UNION := \
+    file_contexts \
+    file.te \
+    device.te \
+    domain.te
+
+# NvCamera extensions
+TARGET_SPECIFIC_HEADER_PATH := device/htc/tegra3-common/include
+
+# Hardware tunables
+BOARD_HARDWARE_CLASS := device/htc/tegra3-common/cmhw/
